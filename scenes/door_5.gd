f42e3ef2fb2bd70f59ca6278d2d9d53df90720f1
plouaincet <maiasswag@gmail.com>
2026-08-12 02:42:45 +0300

    doors, dynamic patrol points and speed bar

---
+++ scenes/door_5.gd
@@ -0,0 +1,20 @@
+extends Doors
+var fade_tween: Tween
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var game: Node2D = $"../.."
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func open(_player: Node) -> void:
+	game.Orange_Doors-=1
+	game.Orange_Door1=false
+	fade_out_door()
+
+func fade_out_door() -> void:
+	fade_tween = create_tween()
+	fade_tween.tween_property(sprite_2d, "modulate:a", 0.0, 0.5)
+	fade_tween.tween_callback(queue_free)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ scenes/doors.gd
@@ -0,0 +1,5 @@
+class_name Doors
+extends StaticBody2D
+
+func open(_player: Node) -> void:
+	pass
