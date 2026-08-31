f0b6a29bd77f2792a1f580d30d0ac7be2d96d425
plouaincet <maiasswag@gmail.com>
2026-09-01 00:19:53 +0300

    graffities and hiddencorridor logic
    * fixed smth
    
    * graffities and hiddencorridor logic

---
+++ scenes/graffities.gd
@@ -0,0 +1,5 @@
+class_name Graffiti
+extends Node2D
+
+func wipe(_player: Node) -> void:
+	pass
--- scenes/map.gd
+++ scenes/map.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var enemy: CharacterBody2D = %Enemy
 @onready var CorridorEntrance: StaticBody2D = $"../Doors/Door10"
 @onready var CorridorExit: StaticBody2D = $"../Doors/Door8"
+@onready var CorridorExitArea: Area2D = $"../Doors/Door8/Area2D"
+@onready var CorridorExitAreaCollision: CollisionShape2D = $"../Doors/Door8/Area2D/CollisionShape2D"
+@onready var CorridorEntranceArea: Area2D = $"../Doors/Door10/Area2D"
+@onready var CorridorEntranceAreaCollision: CollisionShape2D = $"../Doors/Door10/Area2D/CollisionShape2D"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -16,12 +20,16 @@ func _process(_delta: float) -> void:
 func _on_corridor_entrance_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		enemy._change_vision_ray(false)
-		enemy.stop_chase=true
+		enemy.stop_chase = true
+		CorridorEntranceArea.set_deferred("monitorable", false)
+		CorridorEntranceAreaCollision.set_deferred("disabled", true)
 		CorridorEntrance.come_back()
 
 
 func _on_corridor_exit_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		enemy._change_vision_ray(true)
-		enemy.stop_chase=false
+		enemy.stop_chase = false
+		CorridorExitArea.set_deferred("monitorable", false)
+		CorridorExitAreaCollision.set_deferred("disabled", true)
 		CorridorExit.come_back()
